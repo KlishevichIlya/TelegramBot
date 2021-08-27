e5e87c2e6910de1a3395ec38a7fcc268c7718b60
Ilya Klishevich <klishevich.ilya@gmail.com>
2021-08-27 16:40:06 +0300

    Started working with HttpClient and TelegramBot

---
+++ ParserAutoRun/Controllers/NovostiController.cs
@@ -0,0 +1,54 @@
+﻿using AngleSharp;
+using AngleSharp.Html.Parser;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ParserAutoRun.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ParserAutoRun.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NovostiController : ControllerBase
+    {
+        private readonly string _url;
+        private readonly HttpClient _client;
+        private readonly List<Article> _articles;
+
+        public NovostiController()
+        {
+            _client = new HttpClient();
+            _url = $"https://autorun.by/novosti/soligorsk/";
+            _articles = new List<Article>();
+        }
+
+        [HttpGet("/getNews")]
+        public async Task<List<Article>> GetHtmlRequest(int page = 1)
+        {
+            var response = await _client.GetAsync($"{_url}?page={page}");
+            if (!response.IsSuccessStatusCode)
+                return null;
+            var source = await response.Content.ReadAsStringAsync();
+            var parser = new HtmlParser();
+            var document = await parser.ParseDocumentAsync(source);           
+           
+            foreach(var node in document.QuerySelectorAll("div.elem-66 div.news-item"))
+            {                
+                var item = new Article
+                {
+                    Href = "https://autorun.by/" + node.QuerySelector("div.item a.thumb").GetAttribute("href"),
+                    Image = "11",
+                    Title = node.QuerySelector("div.item a.thumb img").GetAttribute("alt")
+                };
+                _articles.Add(item);
+            }           
+            return _articles;
+        }
+    }
+}
---
+++ TelegramBot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace TelegramBot
+{
+    public class Program
+    {
+        private static string _token = "1973694233:AAHqgQSqs7lz-TE7n5HVCm5Z692ZRiivQcc";
+        private static TelegramBotClient _client;
+
+        
+        static void Main(string[] args)
+        {
+            _client = new TelegramBotClient(_token);
+            _client.StartReceiving();
+            _client.OnMessage += OnMessageHandler;
+            Console.ReadLine();
+            _client.StopReceiving();
+        }
+
+        private static void OnMessageHandler(object sender, MessageEventArgs e)
+        {
+            if(e.Message != null && e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text && !string.IsNullOrEmpty(e.Message.Text))
+            {
+                try
+                {
+                    if(e.Message.Text.ToLower().Contains("hi"))
+                    {
+                        _client.SendTextMessageAsync(e.Message.Chat.Id, "Proverka");
+                    }
+                    
+                }
+                catch(Exception ex)
+                {
+
+                }
+            }            
+        }
+    }
+}
